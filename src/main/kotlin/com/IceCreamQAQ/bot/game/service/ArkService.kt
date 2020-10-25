1812c67ffed34e296f275afe5736948424b93aaa
x8140 <shadowxiaoruantian@gmail.com>
2020-10-25 20:56:42 +0800

    新增卡池 勿忘我

--- src/main/kotlin/com/IceCreamQAQ/bot/game/service/ArkService.kt
+++ src/main/kotlin/com/IceCreamQAQ/bot/game/service/ArkService.kt
@@ -19,30 +19,30 @@ class ArkService: BaseCardService() {
         return if (r <= (pool.sixProbability + if (sixFloor > 50) (sixFloor - 50) * 0.02 else 0.0)) {
             val c = sixFloor
             sixFloor = 0
-            if (pool.upFloor == null) CardSettle(6, c, pool, false, isUp = false)
+            if (pool.upSixFloor == null) CardSettle(6, c, pool, false, isUp = false)
             else {
                 val rr = Math.random()
-                if (rr > pool.upFloor!!) {
+                if (rr > pool.upSixFloor!!) {
                     CardSettle(6, c, pool, false, isUp = false)
                 } else CardSettle(7, c, pool, isFloor = false, isUp = false)
             }
         } else if (r <= pool.fiveProbability) {
             val c = fiveFloor
             fiveFloor = 0
-            if (pool.upFloor == null) CardSettle(4, c, pool, false, isUp = false)
+            if (pool.upFiveFloor == null) CardSettle(4, c, pool, false, isUp = false)
             else {
                 val rr = Math.random()
-                if (rr > pool.upFloor!!) {
+                if (rr > pool.upFiveFloor!!) {
                     CardSettle(4, c, pool, false, isUp = false)
                 } else CardSettle(5, c, pool, isFloor = false, isUp = false)
             }
         } else if (r <= pool.fourProbability) {
             val c = fourFloor
             fourFloor = 0
-            if (pool.upFloor == null) CardSettle(2, c, pool, false, isUp = false)
+            if (pool.upFourFloor == null) CardSettle(2, c, pool, false, isUp = false)
             else {
                 val rr = Math.random()
-                if (rr > pool.upFloor!!) {
+                if (rr > pool.upFourFloor!!) {
                     CardSettle(2, c, pool, isFloor = false, isUp = false)
                 } else CardSettle(3, c, pool, isFloor = false, isUp = false)
             }
